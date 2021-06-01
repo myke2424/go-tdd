2c988f5b3aef46bf5447025943a2fa765a32df66
myke2424 <myke2424@gmail.com>
2021-05-31 21:04:05 -0400

    more pointers

--- pointers_and_errors/pointers.go
+++ pointers_and_errors/pointers.go
@@ -1,16 +1,41 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-type Wallet struct {
-    // Remember, In Go variables,types,functins etc
-    // that start with lowercase are private OUTSIDE the package it's defined in
-    balance int
+// We can declare methods on our bitcoin type. This is useful for adding domain specific functionality.
+// Lets implement 'Stringer' on bitcoin.
+// This interface lets you define how  your type is printed when using with %s format strings
+// This is analogous to pythons __repr__ or __str__
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w *Wallet) Deposit(money int) {
-   fmt.Printf("memory address of balance in Deposit method: '%v' \n", &w.balance)
-   w.balance += money
+type Wallet struct {
+	// Remember, In Go variables,types,functins etc
+	// that start with lowercase are private OUTSIDE the package it's defined in
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("memory address of balance in Deposit method: '%v' \n", &w.balance)
+	w.balance += amount
+}
+
+// var keyword allows use to define values global to the package.
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
 
 // w is the Reciever variable here
@@ -23,9 +48,29 @@ func (w *Wallet) Deposit(money int) {
 
 // Technically we don't need the pointer here since we're not updating state,
 // we could use a copy of the wallet instead.
-func (w *Wallet) Balance() int {
-    // Go doesn't require us to deference the pointer but we could with:
-    // (*w).balance
-    // Struct pointers are automatically deferenced, they're referred to as struct pointers
-    return w.balance
+func (w *Wallet) Balance() Bitcoin {
+	// Go doesn't require us to deference the pointer but we could with:
+	// (*w).balance
+	// Struct pointers are automatically deferenced, they're referred to as struct pointers
+	return w.balance
 }
+
+// Generally you should keep your method reciever types to be the same for consistancy
+
+// Go lets you create new types from existing ones... Lets create a bitcoin int type.
+
+
+// WRAP UP
+// Go copies values when you pass them to functions/methods so if you need mutate state
+// you'll need a pointer to that "thing" you want to change
+
+// The fact go takes a copy of the value is nice but sometimes you want to pass by reference (pointer) for larger data
+// Or something you would have one instance of (like a database connection)
+
+// Pointers can be nil
+// When a function returns a pointer to something, you need to check if it's nil so you dont raise a runtime exception
+// Nil is useful when you want to describe a value that could be missing
+
+// Errors are the way to signify failue when calling a function/method
+
+// Creating new types from exisiting ones are useful for addng more domain specific meaning to values
--- pointers_and_errors/pointers_test.go
+++ pointers_and_errors/pointers_test.go
@@ -1,21 +1,71 @@
 package pointers
 
 import (
-    "testing"
-    "fmt"
+	"fmt"
+	"testing"
 )
 
 func TestWallet(t *testing.T) {
-    wallet := Wallet{}
 
-    wallet.Deposit(10)
-    got := wallet.Balance()
-    want := 10
+	t.Run("Deposit", func(t *testing.T) {
 
-    fmt.Printf("memory address of balance in test is: '%v' \n", &wallet.balance)
+		wallet := Wallet{}
 
-    if got != want {
-        t.Errorf("got '%d' but want '%d'", got, want)
-    }
+		wallet.Deposit(10)
+		want := Bitcoin(10) // This is our bitcoin type mapped to int
 
+		assertBalance(t, wallet, want)
+		fmt.Printf("memory address of balance in test is: '%v' \n", &wallet.balance)
+
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		err := wallet.Withdraw(Bitcoin(10))
+
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+		assertNoError(t, err)
+
+	})
+
+	t.Run("Withdraw with insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(5)}
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, 5)
+		assertError(t, err, ErrInsufficientFunds.Error())
+	})
+
+}
+
+func assertError(t testing.TB, got error, want string) {
+	t.Helper()
+
+	// t.Fatal will stop the test if it is called. We dont want to make any assertions on a error if there isnt one.
+	if got == nil {
+		t.Fatal("didn't get an error but wanted get one")
+	}
+
+	if got.Error() != want {
+		t.Errorf("got '%q', want '%q'", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	balance := wallet.Balance()
+
+	if balance != want {
+		t.Errorf("got '%s' but want '%s'", balance, want)
+	}
 }
