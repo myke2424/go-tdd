60634711924b208e2e7f24c1cc68ac27b502726d
myke2424 <myke2424@gmail.com>
2021-06-05 13:39:10 -0400

    more maps

--- maps/maps.go
+++ maps/maps.go
@@ -1,13 +1,24 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
 
 // We can return multiple values in GO, e.g. a string/error, remember errors can be nil
 // Errors can be converted to a string with the .Error method
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+
+const (
+ ErrNotFound = DictionaryErr("could not find the word you were looking for")
+ ErrWordExists = DictionaryErr("Word already exists in dict")
+ ErrWordDoesNotExist = DictionaryErr("Cannot update a word that doesnt exist")
+)
+
+type DictionaryErr string
+
+// Here we create our own err type which implements the error interface (Error())
+// This makes the errors more reusable and immutable
+func (e DictionaryErr) Error() string {
+    return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	// Map lookup can return 2 values. The second value is a boolean which indicates if the key was found succesfully.
@@ -19,4 +30,50 @@ func (d Dictionary) Search(word string) (string, error) {
 
 	// Remember nil is considered as an error type, so this return works.
 	return value, nil
+}
+
+func (d Dictionary) Update(word, newValue string) error {
+    _, err := d.Search(word)
+
+    switch err {
+        case ErrNotFound:
+             // It's good to use a precise error here like this
+             return ErrWordDoesNotExist
+        case nil:
+             d[word] = newValue
+        default:
+             return err
+    }
+
+    return nil
+}
+
+func (d Dictionary) Delete (word string) {
+    // Go has a builtin delete keyword that works on maps
+    // It takes two args, the map and key to be deleted
+    // It returns nothing
+    delete(d, word)
+}
+
+// An interesting property of maps is that you can modify them without passing an address to it
+// e.g. &myMap
+
+// Maps can also be nil values.
+// A nil map behaves like a normal map when reading, but attemps to write to a nil map will cause a runtime err
+
+
+func (d Dictionary) Add(key string, value string) error {
+    _, err := d.Search(key)
+
+    switch err {
+    case ErrNotFound:
+         d[key] = value
+    case nil:
+         return ErrWordExists
+    default:
+         return err
+    }
+
+    return nil
+
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -24,6 +24,63 @@ func TestSearch(t *testing.T) {
 
 }
 
+func TestAdd(t *testing.T) {
+
+	t.Run("new word", func(t *testing.T) {
+		dict := Dictionary{}
+		word := "test"
+		definition := "this is a test"
+
+		err := dict.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dict, word, definition)
+
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		defintion := "this is a test"
+
+		// Maps will overwrite values with the new provided value.
+		dict := Dictionary{word: defintion}
+
+		err := dict.Add(word, defintion)
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dict, word, defintion)
+
+	})
+}
+
+func TestUpdate(t *testing.T) {
+     word := "test"
+     def := "this is a test"
+
+     dict := Dictionary{word:def}
+
+     newDef := "updated test"
+     err := dict.Update(word, newDef)
+
+     assertDefinition(t, dict, word, newDef)
+     assertError(t, err, nil)
+}
+
+func TestDelete(t *testing.T) {
+    word := "test"
+    def := "this is a test"
+
+    dict := Dictionary{word: def}
+
+    dict.Delete(word)
+
+    _, err := dict.Search(word)
+
+    if err != ErrNotFound {
+        t.Errorf("expected %q to be deleted", word)
+    }
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
@@ -39,4 +96,19 @@ func assertError(t testing.TB, got, want error) {
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
 	}
+}
+
+func assertDefinition(t testing.TB, dict Dictionary, word, defintion string) {
+	t.Helper()
+
+	got, err := dict.Search(word)
+
+	if err != nil {
+		t.Fatal("Failed to find added word", err)
+	}
+
+	if defintion != got {
+		t.Errorf("got %q but want %q", got, defintion)
+	}
+
 }
